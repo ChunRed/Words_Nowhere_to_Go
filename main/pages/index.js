af4c2814e951a2c612f4a812e3599168e721ffd0
sonicstride <sonicstride.tw@gmail.com>
2024-08-01 22:55:00 +0800

    add bootstrap

--- main/pages/index.js
+++ main/pages/index.js
@@ -1,14 +1,28 @@
 import Head from 'next/head';
 //import styles from '../styles/Home.module.css';
 import Link from 'next/link';
+import "bootstrap/dist/css/bootstrap.css";
+import '../styles/global.css';
+import styles from '../styles/layout.module.css';
 
 export default function Home() {
+
+  const ScannerWeb = (params) => {
+    window.open("https://chunred.github.io/BarcodeTest/client/");
+  }
+
   return (
     <main>
-      <h1>home</h1>
-      <h1 className="title">
-        Read <Link href="/test">this page!</Link>
-      </h1>
+      <div className="container bg-black">
+        <div className="row mt-5">
+          <h1 className='text-center text-light'>home</h1>
+        </div>
+        <div className="row mt-2">
+          <div className="col text-center">
+            <button onClick={ScannerWeb} className='btn btn-block btn-outline-light p-3 '>go to scanner web</button>
+          </div>
+        </div>
+      </div>
     </main>
   );
 }
